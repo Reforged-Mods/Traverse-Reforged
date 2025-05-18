73f0f19e5f7f78fd8850240aa0f223deae82dc7b
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-05-18 19:09:10 -0400

    added traverse items to vanilla tabs

--- src/main/java/com/terraformersmc/traverse/item/TraverseItemGroups.java
+++ src/main/java/com/terraformersmc/traverse/item/TraverseItemGroups.java
@@ -2,7 +2,11 @@ package com.terraformersmc.traverse.item;
 
 import com.terraformersmc.traverse.Traverse;
 import com.terraformersmc.traverse.block.TraverseBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroup.StackVisibility;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -33,11 +37,34 @@ public class TraverseItemGroups {
 
 	public static void onCreativeTabPopulate(BuildCreativeModeTabContentsEvent event) {
 		if (event.getTabKey() == ItemGroups.BUILDING_BLOCKS){
-			event.addAll(Stream.of(FIR_LOG, FIR_WOOD, STRIPPED_FIR_LOG, STRIPPED_FIR_WOOD, FIR_PLANKS, FIR_STAIRS, FIR_SLAB,
-				FIR_FENCE, FIR_FENCE_GATE, FIR_DOOR, FIR_TRAPDOOR, FIR_PRESSURE_PLATE, FIR_BUTTON).map(ItemStack::new).toList());
+			List<Block> list = List.of(FIR_LOG, FIR_WOOD, STRIPPED_FIR_LOG, STRIPPED_FIR_WOOD, FIR_PLANKS, FIR_STAIRS, FIR_SLAB,
+				FIR_FENCE, FIR_FENCE_GATE, FIR_DOOR, FIR_TRAPDOOR, FIR_PRESSURE_PLATE, FIR_BUTTON);
+			for (int i = list.size() - 1; i >= 0; i--) {
+				event.getEntries().putAfter(new ItemStack(Items.CHERRY_BUTTON), new ItemStack(list.get(i)), StackVisibility.PARENT_AND_SEARCH_TABS);
+			}
 		}
 		if (event.getTabKey() == ItemGroups.NATURAL){
-			event.addAll(Stream.of(FIR_LOG, BROWN_AUTUMNAL_SAPLING, ORANGE_AUTUMNAL_SAPLING, RED_AUTUMNAL_SAPLING, YELLOW_AUTUMNAL_SAPLING, FIR_SAPLING).map(ItemStack::new).toList());
+			final ItemStack NATURAL_LEAVES = Items.FLOWERING_AZALEA_LEAVES.getDefaultStack();
+			final ItemStack NATURAL_SAPLING = Items.CHERRY_SAPLING.getDefaultStack();
+			event.getEntries().putAfter(new ItemStack(Blocks.CHERRY_LOG), new ItemStack(FIR_LOG), StackVisibility.PARENT_AND_SEARCH_TABS);
+			List<Block> list = List.of(BROWN_AUTUMNAL_SAPLING, ORANGE_AUTUMNAL_SAPLING, RED_AUTUMNAL_SAPLING, YELLOW_AUTUMNAL_SAPLING, FIR_SAPLING);
+			for (int i = list.size() - 1; i >= 0; i--) {
+				event.getEntries().putAfter(NATURAL_SAPLING, new ItemStack(list.get(i)), StackVisibility.PARENT_AND_SEARCH_TABS);
+			}
+			list = List.of(BROWN_AUTUMNAL_LEAVES, ORANGE_AUTUMNAL_LEAVES, RED_AUTUMNAL_LEAVES, YELLOW_AUTUMNAL_LEAVES, FIR_LEAVES);
+			for (int i = list.size() - 1; i >= 0; i--) {
+				event.getEntries().putAfter(NATURAL_LEAVES, new ItemStack(list.get(i)), StackVisibility.PARENT_AND_SEARCH_TABS);
+			}
+		}
+		if (event.getTabKey() == ItemGroups.FUNCTIONAL){
+			final ItemStack FUNCTIONAL_SIGN = Items.CHERRY_HANGING_SIGN.getDefaultStack();
+			event.getEntries().putAfter(FUNCTIONAL_SIGN, new ItemStack(TraverseItems.FIR_HANGING_SIGN), StackVisibility.PARENT_AND_SEARCH_TABS);
+			event.getEntries().putAfter(FUNCTIONAL_SIGN, new ItemStack(TraverseItems.FIR_SIGN), StackVisibility.PARENT_AND_SEARCH_TABS);
+		}
+		if (event.getTabKey() == ItemGroups.TOOLS){
+			final ItemStack TOOLS_BOAT = Items.CHERRY_CHEST_BOAT.getDefaultStack();
+			event.getEntries().putAfter(TOOLS_BOAT, new ItemStack(TraverseBoatTypes.FIR_CHEST_BOAT), StackVisibility.PARENT_AND_SEARCH_TABS);
+			event.getEntries().putAfter(TOOLS_BOAT, new ItemStack(TraverseBoatTypes.FIR_BOAT), StackVisibility.PARENT_AND_SEARCH_TABS);
 		}
 	}
 }
