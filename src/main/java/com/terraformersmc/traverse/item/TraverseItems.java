011fdfe6388aa3839b9ee25f37727f5d7c4e5d23
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-05-18 18:25:20 -0400

    fixed sign item registration

--- src/main/java/com/terraformersmc/traverse/item/TraverseItems.java
+++ src/main/java/com/terraformersmc/traverse/item/TraverseItems.java
@@ -6,20 +6,27 @@ import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.terraformersmc.traverse.block.TraverseBlocks.FIR_WALL_HANGING_SIGN;
 import static com.terraformersmc.traverse.block.TraverseBlocks.FIR_WALL_SIGN;
 
 public class TraverseItems {
-	public static final Item FIR_SIGN = TraverseItems.add("fir_sign", new SignItem(new Item.Settings().maxCount(16), TraverseBlocks.FIR_SIGN, FIR_WALL_SIGN));
-	public static final Item FIR_HANGING_SIGN = TraverseItems.add("fir_hanging_sign", new HangingSignItem(TraverseBlocks.FIR_HANGING_SIGN, FIR_WALL_HANGING_SIGN, new Item.Settings().maxCount(16)));
+	public static final List<Item> ITEMS = new ArrayList<>();
+	public static Item FIR_SIGN;
+	public static Item FIR_HANGING_SIGN;
 	public static <I extends Item> I add(String name, I item) {
+		ForgeRegistries.ITEMS.register(Identifier.of(Traverse.MOD_ID, name), item);
 		if (item instanceof BlockItem blockItem) {
 			blockItem.appendBlocks(Item.BLOCK_ITEMS, item);
 		}
-
-		ForgeRegistries.ITEMS.register(Identifier.of(Traverse.MOD_ID, name), item);
+		ITEMS.add(item);
 		return item;
 	}
 
-	public static void register() {	}
+	public static void register() {
+		FIR_SIGN = TraverseItems.add("fir_sign", new SignItem(new Item.Settings().maxCount(16), TraverseBlocks.FIR_SIGN, FIR_WALL_SIGN));
+		FIR_HANGING_SIGN = TraverseItems.add("fir_hanging_sign", new HangingSignItem(TraverseBlocks.FIR_HANGING_SIGN, FIR_WALL_HANGING_SIGN, new Item.Settings().maxCount(16)));
+	}
 }
